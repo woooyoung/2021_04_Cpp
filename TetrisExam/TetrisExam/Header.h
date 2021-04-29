46221a1a492261cd557627cf95b1ea8ec9226d32
woooyoung <axdsw121@gmail.com>
2021-04-29 11:51:24 +0900

    modify

--- TetrisExam/TetrisExam/Header.h
+++ TetrisExam/TetrisExam/Header.h
@@ -1,15 +1,119 @@
-#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_NO_SECURE_WARNINGS
+#include "tetrisHelper.h"
 
-#include "Tetrishelper.h"
 #include <cstdio>
 #include <cstdlib>
 #include <ctime>
 #include <cstring>
 #include <cmath>
 
+
 #define GRID_WIDTH 7
 #define GRID_HEIGHT 10
+#define USERBLOCK_SIZE 3
 
 int displayData[GRID_HEIGHT][GRID_WIDTH] = { 0, };
 
+class Display {
+public:
+    void draw() {
+        for (int i = 0; i < GRID_HEIGHT; i++) {
+            for (int k = 0; k < GRID_WIDTH; k++) {
+                if (displayData[i][k] == 0) {
+                    drawPosition(k, i, false);
+                }
+                else {
+                    drawPosition(k, i, true);
+                }
+            }
+        }
+    }
+};
 
+class GameEngine {
+public:
+    int gameGridData[GRID_HEIGHT][GRID_WIDTH] = {
+        {0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0},
+        {0,0,1,0,0,0,0},
+        {1,1,1,0,0,0,0},
+        {1,1,1,0,0,0,0},
+    };
+    int userBlock[USERBLOCK_SIZE][USERBLOCK_SIZE] = {
+        {1, 1, 1},
+        {0, 0, 1},
+        {0, 0, 1}
+    };
+    int blockX = 0;      //유저블록의 현재 위치를 기억할 변수
+    int blockY = 0;
+
+    float elapsed = 0.0f;
+
+    void init() {
+        //최초에 게임 엔진을 초기화 하는 과정을 맡는다.
+    }
+    void next(float dt, char keyboardInput) {
+        //blockY++;
+        elapsed = elapsed + dt;      //elapsed += dt; 이거랑 같죠
+        if (elapsed >= 0.5f) {
+            blockY++;
+            elapsed = elapsed - 0.5f;   //elapsed -= dt; 이것도 같아요
+        }
+    }
+
+    //블록이 아래로 내려갈 수 있냐
+    bool canGoDown() {
+        for (int i = 0; i < USERBLOCK_SIZE; i++) {
+            for (int k = 0; k < USERBLOCK_SIZE; k++) {
+                if (userBlock[i][k] == 1 && i + blockY + 1 >= GRID_HEIGHT) {
+                    return false;
+
+                    if (userBlock[i][k] == 1 && gameGridData[i + blockY + 1][k + blockX] == 1) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+    }
+
+    //블록이 왼쪽으로 갈 수 있냐
+    bool canGoLeft() {
+        return true;
+    }
+    //블록이 오른쪽으로 갈 수 있냐
+    bool canGoRight() {
+        return true;
+    }
+    //실제 게임 데이터를 화면에 출력할 수 있는 데이터로 바꿔주는 함수
+    void makeDisplayData() {
+        for (int i = 0; i < GRID_HEIGHT; i++) {
+            for (int k = 0; k < GRID_WIDTH; k++) {
+                displayData[i][k] = gameGridData[i][k];
+            }
+        }
+
+        for (int i = 0; i < USERBLOCK_SIZE; i++) {
+            for (int k = 0; k < USERBLOCK_SIZE; k++) {
+                if (i + blockY < 0 || i + blockY > GRID_HEIGHT) {
+                    //DO NOTHING
+                }
+                else if (k + blockX < 0 || k + blockX > GRID_WIDTH) {
+                    //DO NOTHING
+                }
+                else {
+                    ///displayData[i + blockY][k + blockX] = userBlock[i][k] == 1 ? userBlock[i][k] : displayData[i + blockY][k+ blockX];
+                    int _x = k + blockX;
+                    int _y = i + blockY;
+
+                    displayData[_y][_x] = userBlock[i][k] | displayData[_y][_x];
+                }
+            }
+        }
+    }
+};
--- TetrisExam/TetrisExam/Source.cpp
+++ TetrisExam/TetrisExam/Source.cpp
@@ -1,30 +1,39 @@
 #include "Header.h"
 
-#define INTERVAL 1.0/60.0
+#define INTERVAL 1.0/60.0            
 
 int main() {
 
-	float prev = (float)clock() / CLOCKS_PER_SEC;
+    float prev = (float)clock() / CLOCKS_PER_SEC;
+    Display* display = new Display();
+    GameEngine* gameEngine = new GameEngine();      ///1
+    showConsoleCursor(false);
 
-	while (true) {
-		float curr = (float)clock() / CLOCKS_PER_SEC;
-		float dt = curr - prev;
+    while (true) {
+        float curr = (float)clock() / CLOCKS_PER_SEC;
+        float dt = curr - prev;
 
-		if (dt < INTERVAL) continue;	//정해주는 1/60 보다 작다면 처음으로 돌아가게
-		prev = curr;
+        if (dt < INTERVAL)   continue;
+        prev = curr;
 
-		printf("%.3f\n", curr);
+        //printf("%.3f\n", curr);
 
-		bool left = keyState('a');
-		if (left) {
-			//왼쪽으로 블록 이동
-		}
-		else {
-			//그냥 블록 떨어지게
-		}
-		//화면 출력
-		//게임상태 판별
-	}
+        bool left = keyState('a');
+        if (left) {
+            //왼쪽으로 블록 이동
+        }
+        else {
+            // 그냥 블록 떨어지게
+        }
 
-	return 0;
+        gameEngine->next(dt, 0);            ///3
+
+        //화면 출력
+        gameEngine->makeDisplayData();   /////2
+        display->draw();
+
+        //게임 상태 판별
+    }
+
+    return 0;
 }
